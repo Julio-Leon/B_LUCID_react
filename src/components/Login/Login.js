51318d2c3e77fadf549a573b03df5d3a64421cda
julileon10 <julio.leon.diaz1@gmail.com>
2021-08-10 23:49:26 -0400

    final commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -55,8 +55,14 @@ function Login() {
         }
     }
 
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
     useEffect(() => {
-        if (Cookies.get('jwt')) {
+        if (getCookie('jwt')) {
             console.log('HERE')
             setTimeout(() => {
                 const cookiesArray = document.cookie.split(' ')
@@ -68,9 +74,13 @@ function Login() {
                 localStorage.setItem('token', jwtFinal)
                 localStorage.setItem('id', decodedToken.data._id)
                 userContext.setLoggedIn(true)
-                // history.push('/')
             }, 1500)
-        }   
+        } else {
+            console.log('HEREv2')
+            setTimeout(() => {
+                console.log(document.cookie, getCookie('jwt'))
+            }, 5000)
+        }
     }, [])
 
     return (
@@ -97,11 +107,11 @@ function Login() {
                 <div className="register-user">
                     <Link to='/user/register'>Register</Link>
                 </div>
-                <div class="google-btn">
-                    <div class="google-icon-wrapper">
-                        <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='google icon' />
+                <div className="google-btn">
+                    <div className="google-icon-wrapper">
+                        <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt='google icon' />
                     </div>
-                    <a href="https://boiling-caverns-35260.herokuapp.com/users/google" class="btn-text"><b>Sign in with google</b></a>
+                    <a href="https://boiling-caverns-35260.herokuapp.com/users/google" className="btn-text"><b>Sign in with google</b></a>
                 </div>
                 {/* <a href="http://localhost:4000/users/google"> Google Login </a> */}
                 {/* <button onClick={_handleGoogleLogin}>Google Login</button> */}
